215dfcbb2399a8f2fdd6d0f0386099fd9dbec10c
david <david.lucky.star@gmail.com>
2013-06-11 06:13:58 -0700

    SSI exp

--- js/script.js
+++ js/script.js
@@ -3,4 +3,13 @@ $(function() {
         $(this).tab('show');
     });
     $('#nowSection').addClass('active');
+
+    // past page
+    $('#pastSection').children().hide();
+    $('#pastSection :first-child').show();
+    $('#pastProducts a').click(function() {
+        $('#pastProducts').parent().hide();
+        var target = $(this).next().val();
+        $(target).show();
+    })
 });
